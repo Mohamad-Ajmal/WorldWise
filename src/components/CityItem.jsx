014bae26469fea9e0adf2ac4c9fe513a769da2cb
Mohamad-Ajmal <Ajmalibrahimkhail121@gmail.com>
2024-11-03 13:48:58 +0430

    Consuming ContextAPI

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import styles from './CityItem.module.css';
 import { Link } from 'react-router-dom';
+import { useCities } from '../contexts/CitiesContext';
 
-export default function CityItem({city}) {
+export default function CityItem({city}) { 
+    const {currentCity} = useCities();
     const {cityName, emoji, date, id, position} = city;
     const formatDate = (date) =>
         new Intl.DateTimeFormat("en", {
@@ -12,7 +14,7 @@ export default function CityItem({city}) {
         }).format(new Date(date));
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+      <Link className={`${styles.cityItem} ${id===currentCity.id ? styles["cityItem--active"] : "" }`} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
         <span  className={styles.emoji}>{emoji}</span> 
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
